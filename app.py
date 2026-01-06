5c58e4479a9c50019800057ab5da7c3a655ca756
fmc2 <fciaglia@math.uc3m.es>
2026-01-06 22:47:19 +0100

    chore: add favicon and changed page name

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-st.set_page_config(page_title="DOPT Finance", layout="wide")
-st.title("DOPT Finance Dashboard")
+st.set_page_config(page_title="Database of Poveruomic Transactions", page_icon="\U0001F4B6", initial_sidebar_state="auto", layout="wide")
+st.title("DoPT Finance Dashboard")
 st.write("Use the sidebar to navigate between pages.")
 
 if hasattr(st.sidebar, "page_link"):
